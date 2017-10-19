8dc8292a167559cc434a74978057675afad43c5c
LacosteR <pavel.shuckii@yandex.ru>
2017-10-19 09:09:58 +0200

    Обновление

--- gameserver/GameServer_loop.sh
+++ gameserver/GameServer_loop.sh
@@ -2,7 +2,7 @@
 
 while :;
 do
-	java -server -Dfile.encoding=UTF-8 -Xmx4096m -Xmx8192m -cp config:./../lib/* l2p.gameserver.BootManager > log/stdout.log 2>&1
+	java -client -Dfile.encoding=UTF-8 -Xms4096m -Xmx8192m -XX:+PrintGCDetails -Xloggc:gc.log -cp config:./../lib/smrt.jar:./../lib/smrt-core.jar:./../lib/* ru.akumu.smartguard.SmartGuard l2p.gameserver.BootManager > log/stdout.log 2>&1
 
 	[ $? -ne 2 ] && break
 	sleep 30;
